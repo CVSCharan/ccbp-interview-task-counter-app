59711a280ee860ac2a2299395e876bf68cda2da2
CVSCharan <charan.cvs@gmail.com>
2022-08-25 02:32:41 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    counter: 100,
+  };
+
+  render() {
+    const regDate = new Date("2022", "07", "23", "12", "00", "00");
+    const currentDate = new Date();
+    const { counter } = this.state;
+    const diffTime = Math.abs(currentDate - regDate);
+    this.setState({ counter: diffTime * 6 });
+
+    return (
+      <div className="app">
+        <h1 className="heading1">Counter App</h1>
+        <div className="reg-date">
+          <p className="para1">The Regestration Starts From :</p>
+          <div className="reg-date">
+            <p className="para1">
+              {regDate.getDate()} / 0{regDate.getMonth() + 1} /{" "}
+              {regDate.getFullYear()}
+            </p>
+            <p className="para1">
+              {regDate.getHours()} : {regDate.getMinutes()}0 :{" "}
+              {regDate.getSeconds()}0 AM
+            </p>
+          </div>
+        </div>
+        <p className="para2">Number of Registrations Counter: {counter}</p>
+        <p className="para2">{counter}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
